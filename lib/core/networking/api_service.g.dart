02ced661b19149ea03d47d598b57e11e7cedf8b6
Mohammed Adel <57016916+MoAdelDev@users.noreply.github.com>
2024-02-17 00:43:49 -0800

    Login data created

--- lib/core/networking/api_service.g.dart
+++ lib/core/networking/api_service.g.dart
@@ -20,6 +20,34 @@ class _ApiService implements ApiService {
 
   String? baseUrl;
 
+  @override
+  Future<LoginResponseBody> login(LoginRequestBody loginRequestBody) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(loginRequestBody.toJson());
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<LoginResponseBody>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              'login',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = LoginResponseBody.fromJson(_result.data!);
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
