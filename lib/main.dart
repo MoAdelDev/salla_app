d7952c1fcb0a4826ebcb8a34fa9de32329911965
Mohammed Adel <57016916+MoAdelDev@users.noreply.github.com>
2024-02-18 13:00:19 -0800

    The app uploaded to firebase app distribution

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:salla_app/core/di/dependency_injection.dart';
 import 'package:salla_app/core/helpers/cache_helper.dart';
 import 'package:salla_app/core/router/app_router.dart';
 import 'package:salla_app/salla_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await setupGetIt();
   await ScreenUtil.ensureScreenSize();
   await CacheHelper.init();
