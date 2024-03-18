2bac4d33bf66d48a9b06680c11f914b2f8e2cfca
Mohammed Adel <m.adeldev10@gmail.com>
2024-03-18 04:33:14 -0700

    - Added main file for development and production . - Added launch json for flavor configs . - Added development and production flavors in build gradle . - Changed android app name based on flavor name . - Remove main file .

--- lib/salla_app.dart
+++ lib/salla_app.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:salla_app/core/helpers/cache_helper.dart';
 import 'package:salla_app/core/router/app_router.dart';
 import 'package:salla_app/core/router/routes.dart';
 import 'package:salla_app/core/style/themes.dart';
 import 'package:salla_app/generated/l10n.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class SallaApp extends StatelessWidget {
   final AppRouter appRouter;
