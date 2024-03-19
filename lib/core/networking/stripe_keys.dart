04a51838898858b50dd91fec3725a179c5de6867
Mohammed Adel <m.adeldev10@gmail.com>
2024-03-19 04:49:36 -0700

    - Added Published key and Secret Key for Stripe .

---
+++ lib/core/networking/stripe_keys.dart
@@ -0,0 +1,9 @@
+class StripeKeys {
+  StripeKeys._();
+
+  static const String publishableKey =
+      'pk_test_51OvrgJCcIsF3vpiRCeZFTZdDlF0fmMxVe3pV6SBOTT4T5fALBNHTOqdtnqtBFZS1hRAHict6MBKTpnZ4DQAhmwbP00dFPgnv7w';
+
+  static const String secretKey =
+      'sk_test_51OvrgJCcIsF3vpiRpGciHYOLWe7oytOInqOj8EGXIOyW69WbE2QC5c0l8ckKL6F8pB4VO2JgZBt7BsnCh0q2NgWw00x6QpXBur';
+}
