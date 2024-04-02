27eacc43f8ac99241477505ebe3a0020d1779dba
Mohammed Adel <57016916+MoAdelDev@users.noreply.github.com>
2024-04-02 04:51:55 -0700

    - Changed Splash Icon and App Icon

--- lib/salla_app/salla_app.dart
+++ lib/salla_app/salla_app.dart
@@ -32,7 +32,7 @@ class SallaApp extends StatelessWidget {
             child: MaterialApp(
               debugShowCheckedModeBanner: false,
               theme: lightTheme(context),
-              initialRoute: token != '' ? Routes.home : Routes.onBoarding,
+              initialRoute: token == '' ? Routes.onBoarding : Routes.home,
               onGenerateRoute: (settings) =>
                   appRouter.onGenerateRoute(settings),
               locale: locale,
