13861b15bb5a13ee44581b7bea436c0070c81603
Mohammed Adel <57016916+MoAdelDev@users.noreply.github.com>
2024-04-17 04:43:54 -0700

    Bumped version to 1.2.8

--- lib/features/splash/ui/screens/splash_screen.dart
+++ lib/features/splash/ui/screens/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:salla_app/core/helpers/extensions.dart';
-import 'package:salla_app/features/splash/ui/widgets/splash_brand.dart';
 import 'package:salla_app/features/splash/ui/widgets/splash_logo.dart';
 import 'package:salla_app/features/splash/ui/widgets/splash_title.dart';
 
@@ -19,21 +18,14 @@ class SplashScreen extends StatelessWidget {
             padding: EdgeInsets.symmetric(
               horizontal: 20.0.w,
             ),
-            child: const Column(
-              children: [
-                Expanded(
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SplashTitle(),
-                        SplashLogo(),
-                      ],
-                    ),
-                  ),
-                ),
-                SplashBrand(),
-              ],
+            child: const Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SplashTitle(),
+                  SplashLogo(),
+                ],
+              ),
             ),
           ),
         ),
